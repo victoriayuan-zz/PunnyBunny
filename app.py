8fe20db3b96ce53ebc49ec0ccb08bb8a585d6736
sarinaxue <sarinaxue@users.noreply.github.com>
2017-03-12 15:50:31 -0400

    moree

--- app.py
+++ app.py
@@ -67,7 +67,10 @@ def webhook():
                             "Why did the bunny build herself a new house? she we fed up with the hole thing.",
                             "Why did the bunny bring toilet paper to the party? Because he is a party pooper!",
                             "What happened when 100 hares got loose on Main Street? The police had to comb the area.",
-                            "What do they call a bunny transformer? Hop-timous Prime."]
+                            "What do they call a bunny transformer? Hop-timous Prime."
+                            "When does a bunny chemist tell jokes? Periodically."
+                            "Did you hear about the bunny that had his left side cut off? He is all right now!"
+                            "Did you hear about that bunny with a taser> Man, she was stunning!"]
                     send_message(sender_id, random.choice(puns))
                 
                 if messaging_event.get("delivery"):  # delivery confirmation
