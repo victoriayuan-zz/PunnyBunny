8757e05d823b4dbf498146a687f32a917f780660
victoriayuan <victoriayuan8@gmail.com>
2017-03-11 22:22:12 -0500

    Test5

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import sys
 import json
 
-import random
 import requests
 from flask import Flask, request
 
@@ -17,54 +16,47 @@ def verify():
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-
+    
     return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
 def webhook():
-
+    
     # endpoint for processing incoming messaging events
-
+    
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
-
+    
     if data["object"] == "page":
         
-        #send_message(sender_id, "Hello, I'm Punny Bunny. I can tell you a funny bunny pun. Do you wanna hear it?")
-             
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
-
+                
                 if messaging_event.get("message"):  # someone sent us a message
-
+                    
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    puns = ['Every bunny was kungfu fighting~', 'I’d make a veggie pun but no one would carrot all', 'I whip my hare back and forth~', 'And now he’s just some bunny that I used to know~',
-                            'How do you know if a rabbit is getting old? It’s the gray hare.', 'You seem to be having a bad hare day']
                     
-                    #from random import randrange
-                    #random_index = randint(0,len(puns)-1)
-                    send_message(sender_id, "a"])
-
+                    send_message(sender_id, "HI I'M PUNNY BUNNY! <3")
+                
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
-
+                
                 if messaging_event.get("optin"):  # optin confirmation
                     pass
-
+                
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     pass
 
-    return "ok", 200
+return "ok", 200
 
 
 def send_message(recipient_id, message_text):
-
+    
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
+    
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
     }
@@ -72,14 +64,14 @@ def send_message(recipient_id, message_text):
         "Content-Type": "application/json"
     }
     data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+                      "recipient": {
+                      "id": recipient_id
+                      },
+                      "message": {
+                      "text": message_text
+                      }
+                      })
+r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
@@ -88,6 +80,10 @@ def send_message(recipient_id, message_text):
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
     sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
